4b7aa206290b6dd92cbe8554d372ea3582eafc14
arinkal12 <evgenika4@yandex.ru>
2024-10-02 13:44:41 +0300

    cvy

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,32 +1,69 @@
-
+#include <cstdint>
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <limits>
+#include <cstring>
 template<typename T>
 class ArrayHandler {
 private:
     size_t _size;
     T* _array;
+    T _max;
+    T _min;
+    long long int _count;
 public:
     ArrayHandler(size_t size = 10) {
-
+        _size = size;
+        _array = new T[_size];
+        _max = std::numeric_limits<T>::min();
+        _min = std::numeric_limits<T>::max();
+        _count = 0;
     }
 
     void AppendElem(T elem) {
-
+        if (_count == _size) {
+            _size += 10;
+            T* new_arr = new T[_size];
+            std::memcpy(new_arr, _array, _count * sizeof(T));
+            delete[] _array;
+            _array = new_arr;
+        }
+        _array[_count] = elem;
+        _count++;
+        if (elem > _max) {
+            _max = elem;
+        }
+        if (elem < _min) {
+            _min = elem;
+        }
     }
 
     bool IsContains(T elem) {
-        return false;
+        int l = 0, r = _count - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (_array[mid] == elem) {
+                return mid;
+            } else if (_array[mid] < elem) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+        
     }
 
     T GetMax() {
-        return 0;
+        return _max;
     }
 
     T GetMin() {
-        return 0;
+        return _min;
     }
 
     ~ArrayHandler() {
-
+        delete[] _array;
     }
-
 };
